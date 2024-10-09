34133a25b83e59fa4bff7205d2eba2eea2f8a2b4
Jack <62919016+JackDunfey@users.noreply.github.com>
2024-10-09 12:08:06 -0400

    patch

--- utils.h
+++ utils.h
@@ -2,7 +2,7 @@
 #define utils_H
 
 bool trim_newline(char *s);
-static char **split_on_substring(const char *input, const char *delimiter, int *token_count);
-static void free_tokens(char **tokens, int token_count);
+char **split_on_substring(const char *input, const char *delimiter, int *token_count);
+void free_tokens(char **tokens, int token_count);
 
 #endif
